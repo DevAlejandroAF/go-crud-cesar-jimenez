d0e46c29098346009ef984efbaf86f1b6f9cdf8a
devalejandroaf <alejandroavi95@hotmail.com>
2026-02-19 19:01:36 -0500

    Second commit

--- internal/service/book_service.go
+++ internal/service/book_service.go
@@ -1,1 +1,48 @@
 package service
+
+import (
+	"errors"
+
+	"github.com/devalejandroaf/crud/internal/model"
+	"github.com/devalejandroaf/crud/internal/store"
+)
+
+type Service struct {
+	store store.Store
+}
+
+func New(s store.Store) *Service {
+	return &Service{
+		store: s,
+	}
+}
+
+func (s *Service) GetAllBooks() ([]*model.Book, error) {
+	books, err := s.store.GetAll()
+	if err != nil {
+		return nil, err
+	}
+	return books, nil
+}
+
+func (s *Service) GetBookById(id int) (*model.Book, error) {
+	return s.store.GetByID(id)
+}
+
+func (s *Service) CreateBook(book model.Book) (*model.Book, error) {
+	if book.Title == "" {
+		return nil, errors.New("Title can not be empty")
+	}
+	return s.store.Create(&book)
+}
+
+func (s *Service) UpdateBook(id int, book model.Book) (*model.Book, error) {
+	if book.Title == "" {
+		return nil, errors.New("Title can not be empty")
+	}
+	return s.store.Update(id, &book)
+}
+
+func (s *Service) RemoveBook(id int) error {
+	return s.store.Delete(id)
+}
---
+++ internal/transport/book_handler.go
@@ -0,0 +1,49 @@
+package transport
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/devalejandroaf/crud/internal/model"
+	"github.com/devalejandroaf/crud/internal/service"
+)
+
+type BookHandler struct {
+	service *service.Service
+}
+
+func New(s *service.Service) *BookHandler {
+	return &BookHandler{
+		service: s,
+	}
+}
+
+func (h *BookHandler) HandleBooks(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		books, err := h.service.GetAllBooks()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(books)
+	case http.MethodPost:
+		var book model.Book
+		err := json.NewDecoder(r.Body).Decode(&book)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		created, err := h.service.CreateBook(book)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusCreated)
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(created)
+	default:
+		http.Error(w, "Method no allowed.", http.StatusMethodNotAllowed)
+	}
+}
